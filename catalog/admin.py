cda5c7fc39280b7c72aa24b3af15b8e54987a732
Mugo <keithgety@gmail.com>
2022-08-24 12:54:36 +0300

    err

--- catalog/admin.py
+++ catalog/admin.py
@@ -3,7 +3,7 @@ from . import models
 # Register your models here.
 
 admin.site.register(models.Category)
-admin.site.register(models.Subcategory)
+# admin.site.register(models.Subcategory)
 admin.site.register(models.Product)
 admin.site.register(models.ProductReview)
 admin.site.register(models.OrderItem)
