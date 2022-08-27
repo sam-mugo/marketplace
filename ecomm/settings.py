d8b4d3d7f231b45911a9ebfadf257a1db5a99c76
Mugo <keithgety@gmail.com>
2022-08-27 19:55:15 +0300

    del pycache

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -150,17 +150,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-VENV_PATH = os.path.dirname(BASE_DIR)
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(VENV_PATH, 'static_root')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# VENV_PATH = os.path.dirname(BASE_DIR)
+# STATIC_URL = 'static/'
+# STATIC_ROOT = os.path.join(VENV_PATH, 'static_root')
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+# MEDIA_URL = '/media/'
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+# STATIC_URL = 'static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
